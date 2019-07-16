9595b78588e2cea1f9b8e5d6d4a04762d7049309
WiseJoo <margieross1120@gmail.com>
2019-07-16 15:49:01 +0900

    led03 : separately on/off

--- led03.c
+++ led03.c
@@ -1,5 +1,67 @@
 #include<stdio.h>
+#include<wiringPi.h>
+
+#define LEDRED 0
+#define LEDGRE 2
+#define LEDYEL 3
 
 int main()
 {
+  if(wiringPiSetup() == -1)
+    {
+      printf("setup wiringPi failed!");
+      return 1;
+    }
+
+  pinMode(LEDRED, OUTPUT);
+  pinMode(LEDGRE, OUTPUT);
+  pinMode(LEDYEL, OUTPUT);
+  while(1)
+    {
+      digitalWrite(LEDRED, LOW);
+      digitalWrite(LEDGRE, LOW);
+      digitalWrite(LEDYEL, LOW);
+      printf("ALL LED OFF...\n");
+
+      delay(500);
+      
+      digitalWrite(LEDRED, HIGH);
+      printf("LED RED ON...\n");
+
+      delay(500);
+
+      digitalWrite(LEDRED, LOW);
+      printf("LED RED OFF...\n");
+
+      delay(500);
+
+      digitalWrite(LEDGRE, HIGH);
+      printf("LED GRE ON...\n");
+
+      delay(500);
+
+      digitalWrite(LEDGRE, LOW);
+      printf("LED GRE OFF...\n");
+
+      delay(500);
+
+      digitalWrite(LEDYEL, HIGH);
+      printf("LED YEL ON...\n");
+
+      delay(500);
+
+      digitalWrite(LEDYEL, LOW);
+      printf("LED YEL OFF...\n");
+
+      delay(500);
+      
+      digitalWrite(LEDRED, HIGH);
+      digitalWrite(LEDGRE, HIGH);
+      digitalWrite(LEDYEL, HIGH);
+      printf("All LED ON...\n");
+      
+      delay(500);
+    }
+
+  return 0;
 }
