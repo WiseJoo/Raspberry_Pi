bb7a615f90ba6544e87ce6e76d2dfbb11e6ce3da
WiseJoo <margieross1120@gmail.com>
2019-07-16 14:42:28 +0900

    Second led control program.

---
+++ led02.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<wiringPi.h>
+
+#define LEDRED 0
+#define LEDGRE 2
+#define LEDYEL 3
+
+int main()
+{
+  if(wiringPiSetup() == -1)
+    {
+      printf("setup wiringPi failed!");
+      return 1;
+    }
+
+  pinMode(LEDRED, OUTPUT);
+  pinMode(LEDGRE, OUTPUT);
+  pinMode(LEDYEL, OUTPUT);
+  while(1)
+    {
+      digitalWrite(LEDRED, LOW);
+      digitalWrite(LEDGRE, LOW);
+      digitalWrite(LEDYEL, LOW);
+      printf("LED OFF...\n");
+
+      delay(100);
+
+      digitalWrite(LEDRED, HIGH);
+      digitalWrite(LEDGRE, HIGH);
+      digitalWrite(LEDYEL, HIGH);
+      printf("LED ON...\n");
+
+      delay(100);
+    }
+
+  return 0;
+}
