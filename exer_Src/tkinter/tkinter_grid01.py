a17c5a8b55be321d886f10b76aa29c6353f038aa
WiseJoo <margieross1120@gmail.com>
2019-09-04 14:47:58 +0900

    grid exercise 01 - Collocate in grid form

---
+++ exer_Src/tkinter/tkinter_grid01.py
@@ -0,0 +1,16 @@
+### library import ###
+from tkinter import *
+
+### main code ###
+root = Tk()
+
+lbl = Label(root, text = "Name")
+lbl.grid(row = 0, column = 0)
+
+txt = Entry(root)
+txt.grid(row = 0, column = 0)
+
+btn = Button(root, text = "OK", width = 15)
+btn.grid(row = 1, column = 1)
+
+root.mainloop()
