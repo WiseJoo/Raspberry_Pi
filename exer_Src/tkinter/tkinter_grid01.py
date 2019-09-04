f804fa99789889c48c61d7c20b75bc1bb7fd1907
WiseJoo <margieross1120@gmail.com>
2019-09-04 15:13:11 +0900

    fix txt's location : column 0 -> column 1

--- exer_Src/tkinter/tkinter_grid01.py
+++ exer_Src/tkinter/tkinter_grid01.py
@@ -4,13 +4,13 @@ from tkinter import *
 ### main code ###
 root = Tk()
 
-lbl = Label(root, text = "Name")
+lbl = Label(root, text = 'Name')
 lbl.grid(row = 0, column = 0)
 
 txt = Entry(root)
-txt.grid(row = 0, column = 0)
+txt.grid(row = 0, column = 1)
 
-btn = Button(root, text = "OK", width = 15)
+btn = Button(root, text = 'OK', width = 15)
 btn.grid(row = 1, column = 1)
 
 root.mainloop()
